0bb0d2f3e988b0d57667527b98c99f29b7d5c6b5
Kyle Busey <kyledbuseybusiness@gmail.com>
2023-05-17 17:48:51 -0400

    Connected frontend to backend and restructured app

---
+++ CapstoneUniversity/frontend/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import index
+
+app_name = 'frontend'
+
+urlpatterns = [
+    path('', index, name=''),
+    path('register', index),
+    path('login', index),
+    path('home', index),
+    path('students/feed', index),
+    path('faculty/register', index),
+    path('faculty/feed', index),
+    
+]
