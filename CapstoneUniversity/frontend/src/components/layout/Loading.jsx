d4cc433d5e4a91f9fcf998bc272081d84edd8339
Kyle Busey <kyledbuseybusiness@gmail.com>
2023-10-04 12:16:53 -0400

    Added basic loading component

---
+++ CapstoneUniversity/frontend/src/components/layout/Loading.jsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+
+    return(
+        <>
+        <h2>Loading...</h2>
+        <p>Hold on a second, we have to load a couple things.</p>
+        </>
+    );
+
+
+}
