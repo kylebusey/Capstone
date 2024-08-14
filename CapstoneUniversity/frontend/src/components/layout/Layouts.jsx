c0e23831b69c5536bd231b1806c72e9ae562da16
kylebusey <kyledbuseybusiness@gmail.com>
2024-08-14 14:03:39 -0400

    Changed layout to be more responsive

--- CapstoneUniversity/frontend/src/components/layout/Layouts.jsx
+++ CapstoneUniversity/frontend/src/components/layout/Layouts.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./Navbar";
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
+import './layout.css';
 
 export default function Layout() {
     return (
-    <div>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </div>
+      <div className="main-layout">
+      <header><Navbar /></header>
+      <main><Outlet /></main>
+      <footer><Footer /></footer>
+      </div>
     );
   }
