3c97b6bddb4adaf61a64556f22e102dcf3bf79c1
kylebusey <kyledbuseybusiness@gmail.com>
2024-08-20 16:47:05 -0400

    Redid feed page to add hero pic

--- CapstoneUniversity/frontend/src/components/feed/FacultyFeed.jsx
+++ CapstoneUniversity/frontend/src/components/feed/FacultyFeed.jsx
@@ -7,9 +7,16 @@ export default function FacultyFeedPage() {
   return (
     <div className='main_container'>
 
+        <div className='title'>
+          <h1>FACULTY FEED</h1>
+        </div>
+
+
+        <div className='flex-container'>
+
         <div className='side_panel'>
           <div className='top-descriptor'>
-            <h1>FACULTY FEED</h1>
+            
 
             <h2>Overview</h2>
             <p>
@@ -34,6 +41,7 @@ export default function FacultyFeedPage() {
         <div className='picture-one'>
             <img className='feedpic' alt = "School News 1" src={feedpic1}/>
             <div className='date-header'><h2>October 2nd</h2></div>
+            <hr/>
             <div className='date-subheader'><h3>Library Career Session</h3></div>
             <div className='event-description'><p>Join our career coordinator for an information session at 4:00 PM to go over tips and tricks for finding a job that works for you.
                The Q&A session starts at 5:30 PM. You won't want to miss this!</p></div>
@@ -42,6 +50,7 @@ export default function FacultyFeedPage() {
             <div className='picture-two'>
               <img className='feedpic' alt = "School News 2" src={theatre}/>
               <div className='date-header'><h2>October 25th</h2></div>
+              <hr/>
               <div className='date-subheader'><h3>School Theatre Night</h3></div>
               <div className='event-description'><p>Come see our Theatre group perform a play with showtimes at 6:00 PM and 8:00 PM. Tickets start at $19.99 and can be purchased at the door.</p></div>
             </div>
@@ -49,12 +58,13 @@ export default function FacultyFeedPage() {
             <div className='picture-three'>
               <img className='feedpic' alt = "School News 3" src={ncaa}/>
               <div className='date-header'><h2>November 4th</h2></div>
+              <hr/>
               <div className='date-subheader'><h3>Basketball Game State Championship</h3></div>
               <div className='event-description'><p>Our basketball team is in the state championship and will play at home at 5:30 PM. </p></div>
             </div>
         </div>
 
-      
+        </div>
     </div>
 
   );
